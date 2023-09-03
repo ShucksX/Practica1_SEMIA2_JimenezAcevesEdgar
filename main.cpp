328f0ba031ec81aaae445a457651c0e1129570c7
Shucks <edgarjagh@gmail.com>
2023-09-03 09:35:19 -0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Practica1_SEMIA2_JimenezAcevesEdgar.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
